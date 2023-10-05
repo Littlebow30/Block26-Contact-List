b81e2b5991b28b9156d8f47bf5b8c7ba834a9a54
Littlebow30 <bowmanA1906@gmail.com>
2023-10-04 21:10:51 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { ContactList } from './components/ContactList'
+import ContactRow from './components/ContactRow'
+
+function App() {
+
+
+  return (
+    <>
+     <ContactList />
+    </>
+  )
+}
+
+export default App
